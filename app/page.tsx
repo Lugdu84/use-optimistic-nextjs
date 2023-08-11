81ba5af013d182f4135fb68c3865a8634acb1eca
David Grammatico <david.lugdunum@gmail.com>
2023-08-11 22:23:16 +0200

    update route

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch("http://localhost:3000/likes", {
+    cache: "no-cache",
+    next: {
+      tags: ["likes"],
+    },
+  })
+
+  const { likes } = await res.json()
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-center p-24">
       <h1 className='text-2xl font-bold text-center mb-5'>UseOptimistic Demo</h1>
+      <p>{likes}</p>
     </main>
   )
 }
